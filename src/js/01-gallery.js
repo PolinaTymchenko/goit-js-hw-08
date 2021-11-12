5bd051da33e6c33c5723b1bb558885e0e578cc16
Polina Tymchenko <polina0503800814@gmail.com>
2021-11-12 09:19:25 +0200

    simplelightbox imported

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simple-lightbox";
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+function galleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+                    </a>
+                </li>`;
+    })
+        .join("")
+};
+
+galleryEl.addEventListener("click", onPhotoClick);
+
+function onPhotoClick(event) {
+
+    event.preventDefault();
+
+    const isGalleryImageEl = event.target.classList.contains("gallery__image");
+
+    if (!isGalleryImageEl) {
+        return;
+    }
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+    });
+
+    gallery.on('show.simplelightbox', function () { })
+}
 
 console.log(galleryItems);
