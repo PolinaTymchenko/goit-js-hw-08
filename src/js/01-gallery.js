e5c905267855522c2665ab1db82c2b31eceec01c
Polina Tymchenko <polina0503800814@gmail.com>
2021-11-12 22:05:48 +0200

    first and third task were done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from "simple-lightbox";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const galleryEl = document.querySelector(".gallery");
 
 galleryEl.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
